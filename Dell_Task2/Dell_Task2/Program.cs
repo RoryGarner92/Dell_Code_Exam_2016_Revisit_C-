b9dbe990220fe1b53d26dc401f27f5be8b02b5c6
RoryGarner92 <rorygarner92@gmail.com>
2017-11-19 19:45:30 +0000

    pushing up task2, improved nested loop, and better use of mod10 as a function

---
+++ Dell_Task2/Dell_Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Dell_Task2
+{
+    class Program
+    {   //using mod 10 to reverse the number rather than converting to a string
+        public static bool CheckNumberIsPalindrome(ref int num)
+        {
+            var palindromeValue = false;
+            var checkNumber = num;
+            var reverseNumber = 0;
+
+            while (num > 0)
+            {
+                var remainder = num % 10;
+                reverseNumber = reverseNumber * 10 + remainder;
+                num = num / 10;
+            }
+
+            if (checkNumber == reverseNumber)
+            {
+                palindromeValue = true;
+            }
+
+            return palindromeValue;
+        }
+
+        public static void Main()
+        {
+            var count = 999;
+            var sum = 0;
+
+            for (var i = count; i >= 100; --i) //993
+            {
+                for (var j = count; j >= 100; --j) //913 
+                {
+                    var answer = i * j;
+                    var var1 = answer;
+
+                    if (CheckNumberIsPalindrome(ref var1) && answer > sum)
+                    {
+                        sum = answer;
+                    }
+                }
+                count--;
+            }
+            Console.WriteLine("The largest palindrome, that is also the product of 3 numbers : " + sum);
+            Console.ReadKey();
+        }
+    }
+}
+
