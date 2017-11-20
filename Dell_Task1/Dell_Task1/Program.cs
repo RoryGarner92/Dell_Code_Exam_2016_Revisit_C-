de1906591f9b2a44ffa44ed650818a83afca23ab
RoryGarner92 <rorygarner92@gmail.com>
2017-11-20 09:23:58 +0000

    adding in task 1, some comments and a timer

--- Dell_Task1/Dell_Task1/Program.cs
+++ Dell_Task1/Dell_Task1/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿/*
+ Name :         Rory Garner
+ College :      IT Carlow
+ Course :       Software Development
+ Description :  Looking for the largest factor of a set number
+                
+ Solution :    For this one I had to do a bit of reading to refresh.
+               Worth noting about this number (not devisable by 2 so I can eliminate all the evens )
+               I set my inital factor to 3 for this reason (smallest odd number)
+               I check to see if the number divided into is a factor then increment by 2 hitting all the odd numbers 
+
+               */
+using System;
+using System.Diagnostics;
 
 namespace Dell_Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var longNumber = 600851475143L; 
+            var factor = 3;
+            //var sw = Stopwatch.StartNew();
+        
+            while (longNumber > 1)
+            {
+                if (longNumber % factor != 0)
+                {
+                    factor += 2; 
+                }
+                else
+                {
+                    longNumber = longNumber / factor;
+                }       
+            }
+            //sw.Stop();
+           // Console.WriteLine($"Time to calculate in milliseconds : {sw.ElapsedMilliseconds}");
+            Console.WriteLine("The answer is : " + factor);
+            Console.ReadKey();
         }
     }
 }
--- Dell_Task2/Dell_Task2/Program.cs
+++ Dell_Task2/Dell_Task2/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ Name :         Rory Garner
+ College :      IT Carlow
+ Course :       Software Development
+ Description :  I am looking for a number that is the porduct of two three digit numbers (e.g 999*999),
+                the number must also be a palindrome and the largest 
+                
+ Solution :     I use a nested for loop starting at 999 and working back to 0 (looking for the largest)
+                I pass the product of the index position of the two loops i&j to the function
+                The function uses mod10 to reverse the number and check to see if its a palindrome 
+                I also use a shared variable to initialize the size of the loop which is reduced by one each pass of the inner loop */
+using System;
+using System.Diagnostics;
 
 namespace Dell_Task2
 {
@@ -29,6 +41,7 @@ namespace Dell_Task2
         {
             var count = 999;
             var sum = 0;
+            //var sw = Stopwatch.StartNew();
 
             for (var i = count; i >= 100; --i) //993
             {
@@ -44,6 +57,8 @@ namespace Dell_Task2
                 }
                 count--;
             }
+            //sw.Stop();
+            //Console.WriteLine($"Time to calculate in milliseconds : {sw.ElapsedMilliseconds}");
             Console.WriteLine("The largest palindrome, that is also the product of 3 numbers : " + sum);
             Console.ReadKey();
         }
